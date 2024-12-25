75982656d00a0c44ea9f896fbd4aa800f906692b
Suminan02 <talalajantawong@gmail.com>
2024-12-25 14:04:07 +0700

    learn-go-1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learngolang.com/investment-calculator
+
+go 1.23.4
---
+++ investment_calculator.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Investment Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+
+}
